fa2444083738af84839853b4df3e2906fe2d6e88
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-01-02 15:00:00 +0530

    Add food CRUD operations and routes

--- backend/src/controllers/FoodController.js
+++ backend/src/controllers/FoodController.js
@@ -3,6 +3,8 @@ import { ApiErrors } from "../utils/ApiErrors.js";
 import { ApiResponse } from "../utils/ApiResponseHandler.js";
 import { uploadOnCloudinary } from "../utils/cloundinary.js";
 import { Food } from "../models/Food.model.js";
+import { Category } from "../models/Category.model.js";
+import { v2 as cloudinary } from 'cloudinary'
 
 const addFood = asyncHandler(async (req, res) => {
 
@@ -31,7 +33,7 @@ const addFood = asyncHandler(async (req, res) => {
     const foodImage = await uploadOnCloudinary(foodImageLocalPath, "Food-Image")
 
     if(!foodImage){
-        throw new ApiErrors(400, "Something went wrong while uploading food image")
+        throw new ApiErrors(500, "Something went wrong while uploading food image")
     }
 
     const food = await Food.create({
@@ -43,7 +45,24 @@ const addFood = asyncHandler(async (req, res) => {
     })
 
     if(!food){
-        throw new ApiErrors(400, "Something went wrong while creating food")
+        throw new ApiErrors(500, "Something went wrong while creating food")
+    }
+
+    const categoryToPushFoodIn = await Category.findByIdAndUpdate(
+        food.category,
+        {
+            $push: {
+                value: food._id
+            }
+        },
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+
+    if(!categoryToPushFoodIn){
+        throw new ApiErrors(404, "Category not found")
     }
 
     return res
@@ -57,6 +76,162 @@ const addFood = asyncHandler(async (req, res) => {
     )
 })
 
+const getFoodById = asyncHandler(async (req, res) => {
+    const foodId = req.params._id
+
+    if(!foodId){    
+        throw new ApiErrors(400, "FoodId is not reaching the controller")
+    }
+
+    const food = await Food.findById(foodId)
+
+    if(!food){
+        throw new ApiErrors(404, "Food not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            food,
+            "Food found"
+        )
+    )
+})
+
+const deleteFoodById = asyncHandler(async (req, res) => {
+    const foodId = req.params._id
+
+    //console.log(req.params);
+
+    if(!foodId){    
+        throw new ApiErrors(400, "FoodId is not reaching the controller")
+    }
+
+    const food = await Food.findByIdAndDelete(foodId)
+
+    if(!food){
+        throw new ApiErrors(404, "Food not found")
+    }
+
+    const imageToBeDeleted = food.foodImage;
+
+    const urlParts = imageToBeDeleted.split("/");
+    const n = urlParts.length;
+    const inp = urlParts[n - 2] + "/" + urlParts[n - 1];
+    const newInp = inp.split(".")
+    //console.log(newInp[0]);
+
+    if (newInp) {
+        await cloudinary.api.delete_resources(newInp[0]);
+    }
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            food,
+            "Food deleted"
+        )
+    )   
+})
+
+const foodAvailability = asyncHandler(async (req, res) => {
+    const foodId = req.params._id
+
+    if(!foodId){
+        throw new ApiErrors(400, "FoodId is not reaching the controller")
+    }
+
+    const food = await Food.findById(foodId)
+
+    if(!food){
+        throw new ApiErrors(404, "Food not found")
+    }
+
+    const isAvailable = food.isAvailable
+
+    food.isAvailable = !isAvailable
+
+    await food.save()
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            !isAvailable,
+            "Food availability toggled successfully"
+        )
+    )
+
+})
+
+const updateFoodById = asyncHandler(async (req, res) => {
+
+    const {title, isAvailable, category, description, price } = req.body
+
+    if(!(title || category || description || price)){
+        throw new ApiErrors(400, "Fill anyone field to update")
+    }
+
+    //console.log(req.params);
+
+    const foodId = req.params._id
+
+    if(!foodId){    
+        throw new ApiErrors(400, "FoodId is not reaching the controller")
+    }
+
+    const food = await Food.findByIdAndUpdate(
+        foodId,
+        {
+            title,
+            category,
+            description,
+            price,
+            isAvailable
+        },
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+
+    if(!food){
+        throw new ApiErrors(404, "Food not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            food,
+            "Food updated"
+        )
+    )
+})
+
+const getAllFoods = asyncHandler(async (req, res) => {
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            await Food.find({}),
+            "All foods"
+        )
+    )
+})
+
 export {
-    addFood
+    addFood,
+    getFoodById,
+    deleteFoodById,
+    foodAvailability,
+    updateFoodById,
+    getAllFoods
 }
--- backend/src/routes/Food.route.js
+++ backend/src/routes/Food.route.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { addFood } from "../controllers/FoodController.js";
+import { 
+    addFood,
+    deleteFoodById,
+    foodAvailability,
+    getAllFoods,
+    getFoodById, 
+    updateFoodById 
+} from "../controllers/FoodController.js";
 import { verifyAdmin } from "../middlewares/verifyAdmin.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -7,5 +14,15 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/add-food").post(verifyJWT, verifyAdmin, upload.single("foodImage"), addFood);
+
+router.route("/get-food-by-id/:_id").get(verifyJWT, getFoodById)
+
+router.route("/update-food/:_id").patch(verifyJWT, verifyAdmin, updateFoodById)
+
+router.route("/delete-food/:_id").delete(verifyJWT, verifyAdmin, deleteFoodById);
+
+router.route("/get-all-foods").get(verifyJWT, getAllFoods);
+
+router.route("/food-availability/:_id").patch(verifyJWT, verifyAdmin, foodAvailability);
 
 export default router;
