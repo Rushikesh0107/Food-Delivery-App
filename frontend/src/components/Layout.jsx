930ab095dc5e7fe050e197d45ba92e2c4d4a51c1
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-01-16 00:51:17 +0530

    Add Footer and Header components

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './Header/Hearder.jsx'
-import Footer from './Footer/Footer.jsx'
+import Header from './Common/Header/Header'
+import Footer from './Common/Footer/Footer'
 
 const Layout = () => {
   return (
